840120c233cf93c00d96b026dc5c8470e1c174fc
logovok <logovok093@gmail.com>
2023-05-08 01:43:39 +0300

    Update main.java

--- ParallelStreamTask/main.java
+++ ParallelStreamTask/main.java
@@ -1,16 +1,35 @@
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Random;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,2,3,4,5,6,7,7,7,8,8,9,10, 10, 10, 10};
+        Random r = new Random();
+        int[] arr = r.ints(1,100)
+                .limit(20000000)
+                .toArray();
+        long time = System.currentTimeMillis();
+        System.out.println(ArrayProcessor.getEvenNumbersParallelSorted(arr));
+        time = System.currentTimeMillis() - time;
+        System.out.println(time);
+
+
+        time = System.currentTimeMillis();
         System.out.println(ArrayProcessor.getEvenNumbersSorted(arr));
+        time = System.currentTimeMillis() - time;
+        System.out.println(time);
     }
 }
 class ArrayProcessor{
-    static Map<Integer, Long> getEvenNumbersSorted(int[] arr) {
+    static Map<Integer, Long> getEvenNumbersParallelSorted(int[] arr) {
         return Arrays.stream(arr).parallel().boxed()
+                .filter(item -> item % 2 == 0)
+                .collect(Collectors.groupingBy(item -> item, TreeMap::new, Collectors.counting()));
+    }
+
+    static Map<Integer, Long> getEvenNumbersSorted(int[] arr) {
+        return Arrays.stream(arr).boxed()
                 .filter(item -> item % 2 == 0)
                 .collect(Collectors.groupingBy(item -> item, TreeMap::new, Collectors.counting()));
     }
