6a8da16e7098aa504ac38c9a401161a3a029143a
logovok <logovok093@gmail.com>
2023-05-11 21:18:31 +0300

    Update slowerButUnderstandable.java

--- Threads7Final/slowerButUnderstandable.java
+++ Threads7Final/slowerButUnderstandable.java
@@ -10,9 +10,9 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
     public static void main(String[] args) {
-        var lst = List.of(new ChunkReader("in1.txt"),
-                new ChunkReader("in2.txt"),
-                new ChunkReader("in3.txt"));
+        var lst = List.of(new ChunkReaderAnalyser("in1.txt"),
+                new ChunkReaderAnalyser("in2.txt"),
+                new ChunkReaderAnalyser("in3.txt"));
         var time = System.currentTimeMillis();
         lst.forEach(Thread::start);
 
@@ -23,30 +23,43 @@ public class Main {
                 throw new RuntimeException(e);
             }
         });
-
-
-        while(ChunkProcessor.chunkCount.get() != ChunkProcessor.processedChunkCount.get()){
+        while(ChunkReaderAnalyser.chunkCount.get() != ChunkReaderAnalyser.processedChunkCount.get()){
             //pass
         }
 
         time = System.currentTimeMillis() - time;
-        ChunkProcessor.excs.shutdown();
-        System.out.println(ChunkProcessor.getAvg());
+        ChunkReaderAnalyser.excs.shutdown();
+        System.out.println(ChunkReaderAnalyser.getAvg());
         System.out.println(time);
+
+
+
+
     }
 }
 
-class ChunkReader extends Thread{
+class ChunkReaderAnalyser extends Thread{
     static int chunkLinesCount = 10;
-
+    static AtomicLong spaces = new AtomicLong(0);
+    static AtomicLong letters = new AtomicLong(0);
+    static double getAvg(){
+        System.out.println(letters.get() + " letters");
+        System.out.println(spaces.get() + " spaces");
+        return letters.get()/(spaces.get() + 1.0);
+    }
     File f;
-    public ChunkReader(File file){
+    public ChunkReaderAnalyser(File file){
         f = file;
         start();
     }
-    public ChunkReader(String filename){
+    public ChunkReaderAnalyser(String filename){
         f = new File(filename);
     }
+
+    static ExecutorService excs = Executors.newCachedThreadPool();
+    static AtomicInteger chunkCount = new AtomicInteger(0),
+            processedChunkCount = new AtomicInteger(0);
+
 
     public void run() {
         try (BufferedReader br = Files.newBufferedReader(f.toPath())) {
@@ -63,7 +76,21 @@ class ChunkReader extends Thread{
                         break;
                     }
                 }
-                ChunkProcessor.submit(sb.toString());
+                String forThread = sb.toString();
+                chunkCount.incrementAndGet();
+                excs.submit(() -> {
+                    AtomicBoolean flag = new AtomicBoolean(false);
+                    forThread.chars().forEach((ch) ->{
+                        if ((ch >=65 && ch <= 90) || (ch >= 97 && ch <= 122)){
+                            flag.set(true);
+                            letters.incrementAndGet();
+                        } else if ((ch == ' ' || ch == '.' || ch == '!' || ch == '?') && flag.get()) {
+                            flag.set(false);
+                            spaces.incrementAndGet();
+                        }
+                    });
+                    processedChunkCount.incrementAndGet();
+                });
                 sb = new StringBuilder();
             }
 
@@ -73,33 +100,4 @@ class ChunkReader extends Thread{
         }
     }
 
-}
-
-class ChunkProcessor{
-    static ExecutorService excs = Executors.newCachedThreadPool();
-    static AtomicInteger chunkCount = new AtomicInteger(0),
-            processedChunkCount = new AtomicInteger(0);
-    static AtomicLong spaces = new AtomicLong(0);
-    static AtomicLong letters = new AtomicLong(0);
-    static double getAvg(){
-        System.out.println(letters.get() + " letters");
-        System.out.println(spaces.get() + " spaces");
-        return letters.get()/(spaces.get() + 1.0);
-    }
-    static void submit(String s){
-        chunkCount.incrementAndGet();
-        excs.submit(() -> {
-            AtomicBoolean flag = new AtomicBoolean(false);
-            s.chars().forEach((ch) ->{
-                if ((ch >=65 && ch <= 90) || (ch >= 97 && ch <= 122)){
-                    flag.set(true);
-                    letters.incrementAndGet();
-                } else if ((ch == ' ' || ch == '.' || ch == '!' || ch == '?') && flag.get()) {
-                    flag.set(false);
-                    spaces.incrementAndGet();
-                }
-            });
-            processedChunkCount.incrementAndGet();
-        });
-    }
 }
